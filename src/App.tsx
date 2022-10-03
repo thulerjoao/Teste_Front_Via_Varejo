982d52ef05de8911089208d50cb2a212efae733e
João Pedro Thuler Lima <thuler_lima@hotmail.com>
2022-10-03 09:44:36 -0300

    config: added styled components and react router dom libs

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import Router from './router'
 
 const App = () =>{
-  const [count, setCount] = useState(0)
 
   return (
     <div>
-        <div>Hello World</div>
+        <Router/>
     </div>
   )
 }
